333521971cd0264ad2bd1f06a95d7526557541fe
Sagar Barapatre <b119016@iiit-bh.ac.in>
2020-10-11 16:16:29 +0530

    Update user.js

--- backEnd/models/user.js
+++ backEnd/models/user.js
@@ -6,26 +6,30 @@ mongoose.Promise = global.Promise
 
 // create a Schema
 let Users = new mongoose.Schema({
-	email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-  },
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  passwordConf: {
-    type: String,
-    required: true,
-  }
+	
+	// Here we can add a particular user by allowing them to sign in or sign UP
+	
+	
+// 	email: {
+//     type: String,
+//     unique: true,
+//     required: true,
+//     trim: true
+//   },
+//   username: {
+//     type: String,
+//     unique: true,
+//     required: true,
+//     trim: true
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+//   passwordConf: {
+//     type: String,
+//     required: true,
+//   }
 })
 
 module.exports = mongoose.model('Users', Users)
