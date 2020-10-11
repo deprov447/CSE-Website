ab81559cd048723b67da56cc20f540916fe51563
Sagar Barapatre <b119016@iiit-bh.ac.in>
2020-10-11 16:18:29 +0530

    Update user-controller.js

--- backEnd/controllers/auth/user-controller.js
+++ backEnd/controllers/auth/user-controller.js
@@ -4,9 +4,9 @@ let User = require('../../models/user')
 
 // Save user
 exports.signup = (req, res, next) => {
-
+  // This sections is for signup method
 }
 
 exports.login = (req, res, next) => {
-
+  // This section is for login method
 }
