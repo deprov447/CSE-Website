6f47c6394b6ba163d24b158f48e2a1581b76d6c3
Sagar Barapatre <b119016@iiit-bh.ac.in>
2020-10-11 16:15:30 +0530

    Update form.js

--- backEnd/models/form.js
+++ backEnd/models/form.js
@@ -6,21 +6,26 @@ mongoose.Promise = global.Promise
 
 // create a Schema
 let Detail = new mongoose.Schema({
-	name: {
-		type: String,
-		trim: true,
-		required: "input name, field can not be empty"
-	},
-	email: {
-		type: String,
-		trim: true,
-		required: "can not post empty email"
-	},
-	address: {
-		type: String,
-		trim: true,
-		required: "can not post empty address"
-	}
+	
+	// Can add anything that we want to store in our database...
+	
+	
+	
+// 	name: {
+// 		type: String,
+// 		trim: true,
+// 		required: "input name, field can not be empty"
+// 	},
+// 	email: {
+// 		type: String,
+// 		trim: true,
+// 		required: "can not post empty email"
+// 	},
+// 	address: {
+// 		type: String,
+// 		trim: true,
+// 		required: "can not post empty address"
+// 	}
 })
 
 module.exports = mongoose.model('Detail', Detail)
