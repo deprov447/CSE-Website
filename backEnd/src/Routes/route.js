8ca427b219273355757dbae0c02ef20d046dd45a
deprov447 <ac.10edu@gmail.com>
2020-10-11 15:27:34 +0530

    Added Routes

--- backEnd/src/Routes/route.js
+++ backEnd/src/Routes/route.js
@@ -5,7 +5,23 @@ const path = require("path")
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../.././frontEnd/public/index.html"))
+    res.sendFile(path.join(__dirname, "../../../frontEnd/public/index.html"))
 })
 
+router.get("/seniors",(req,res)=>{
+    res.sendFile(path.join(__dirname, "../../../frontEnd/public/knowYourSeniors.html"))
+})
+
+router.get("/introduce",(req,res)=>{
+    res.sendFile(path.join(__dirname, "../../../frontEnd/public/juniorIntro.html"))
+})
+
+router.get("/gallery",(req,res)=>{
+    res.sendFile(path.join(__dirname, "../../../frontEnd/public/gallery.html"))
+})
+
+
+router.get("/roadmap",(req,res)=>{
+    res.sendFile(path.join(__dirname, "../../../frontEnd/public/roadmap.html"))
+})
 module.exports = router;
