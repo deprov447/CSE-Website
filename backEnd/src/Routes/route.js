e4823f7c0efc3e36809ce0d54a761d3d3ace185d
deprov447 <ac.10edu@gmail.com>
2020-10-12 15:15:13 +0530

    route.js edited for mergeablity

--- backEnd/src/Routes/route.js
+++ backEnd/src/Routes/route.js
@@ -4,6 +4,9 @@ const path = require("path")
 
 const router = express.Router();
 
+//Importing the model for user signUp
+const { userSchema, userModel } = require("../../models/user")
+
 router.get("/", (req, res) => {
     res.render("../frontEnd/public/index")
 })
@@ -23,5 +26,30 @@ router.get("/gallery",(req,res)=>{
 router.get("/roadmap",(req,res)=>{
     res.render("../frontEnd/public/roadmap.ejs")
 })
+
+//Router for creating a new User
+router.post("/users/signUp", async (req, res) => {
+    const user = new userModel(req.body);
+    try {
+        await user.save();
+        const token = await user.authToken(user);
+        res.send({ user, token });
+    } catch (error) {
+        res.sendStatus(500);
+    }
+
+})
+
+//Route for user Login
+router.post("/users/login", async (req, res) => {
+    try {
+        const user = await userModel.verifyUser(req.body.email, req.body.password);
+        const token = await user.authToken(user);
+        res.send({ user, token })
+    } catch (error) {
+        res.sendStatus(500)
+    }
+})
+
 
 module.exports = router;
