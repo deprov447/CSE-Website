8ea22cde5a120b095c859b783330bdc6d85fe841
Sandeepan-Das <sandeepandas2000@gmail.com>
2020-11-28 17:16:30 +0530

    Designing page

--- frontEnd/public/scripts/validate.js
+++ frontEnd/public/scripts/validate.js
@@ -1,20 +1,25 @@
-$("#btn").click(function () {
+$(document).ready(function () {
+    setInterval(() => {
+        $.ajax({
+            type: "GET",
+            url: `${window.location.pathname}/users`,
+            data: "user",
+            cache: false,
+            success: function (data) {
 
-    $.ajax({
-        type: "GET",
-        url: `${window.location.pathname}/users`,
-        data: "user",
-        cache: false,
-        success: function (data) {
-            window.localStorage.setItem("token",data)
-            window.location.href="/"
-        },
-		error: function (xhr, ajaxOptions, thrownError) {
-            alert(`User sign up failed ${xhr.responseText}`)
+                window.localStorage.setItem("token", data)
+                window.location.href = "/"
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert(`User sign up failed ${xhr.responseText}`)
 
-        },
-        
-    });
+            },
+
+        });
+    }, 6000);
+
 })
+
+
 
 console.log(window.location.pathname)
