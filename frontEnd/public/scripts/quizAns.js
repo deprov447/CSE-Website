a3adba2a44dab812bfb0b5e1814d248d41d9a216
deprov447 <ac.10edu@gmail.com>
2020-11-29 12:04:20 +0530

    Quiz Answers Page [WIP]

--- frontEnd/public/scripts/quizAns.js
+++ frontEnd/public/scripts/quizAns.js
@@ -18,3 +18,44 @@ function showData(data) {
         $("#first").show();
     } 
 }
+
+var number=1;
+var maxValue=20
+
+var idIn = document.querySelector("#id-in")
+idIn.setAttribute("max",maxValue)
+idIn.setAttribute("min",1)
+var prev = document.querySelector("#prev")
+var next = document.querySelector("#next")
+var stuName = document.querySelector("#stuName")
+
+function changeEv(number){
+    idIn.value=number;
+    stuName.innerHTML=number;
+    //Add other fields later
+}
+
+
+
+idIn.addEventListener("input",(event)=>{
+    number=event.target.value;
+    if(isNaN(number)){
+        alert("Input a number")
+    }
+    else
+        changeEv(number);
+})
+
+prev.addEventListener("click",()=>{
+    if(number>1){
+        number--;
+        changeEv(number);
+    }
+})
+
+next.addEventListener("click",()=>{
+    if(number<maxValue){
+        number++;
+        changeEv(number);
+    }
+})
