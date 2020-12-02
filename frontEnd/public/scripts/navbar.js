d1056bbbb7c16049ab3c1469eaa56ecbc28f52fe
Anubhav Choudhary <ac.10edu@gmail.com>
2020-12-02 16:51:13 +0530

    Swipe action added

--- frontEnd/public/scripts/navbar.js
+++ frontEnd/public/scripts/navbar.js
@@ -14,4 +14,49 @@ $(document).ready(function () {
       toggleSidebar();
     }
   });
+
+  // document.open()
+  // document.write(unescape("%3Cscript src='https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js%3E'"))
+  // document.close()
+
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+          toggleSidebar();
+        } else {
+          toggleSidebar();
+        }                       
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
 });
